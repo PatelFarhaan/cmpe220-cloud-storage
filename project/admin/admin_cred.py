b2963c1cfcd8f1abced883a038d50a314e219453
Farhaan Patel <patel.farhaaan@gmail.com>
2019-10-07 15:09:11 -0700

    user and admin login fixed and working

--- project/admin/admin_cred.py
+++ project/admin/admin_cred.py
@@ -3,13 +3,15 @@ sys.path.append('../../')
 
 
 from project import db
-from project.admin.models import Admin
+from project.users.models import Users
 from werkzeug.security import generate_password_hash
 
 
 
-admin_obj = Admin(admin_email='patel.***REMOVED_EMAIL***',
-                  admin_hashed_password=generate_password_hash('***REMOVED_PASSWORD***')
+admin_obj = Users(email='patel.***REMOVED_EMAIL***',
+                  hashed_password=generate_password_hash('***REMOVED_PASSWORD***'),
+                  mobile_number='9962048121',
+                  is_admin=True
                   )
 db.session.add(admin_obj)
 db.session.commit()
