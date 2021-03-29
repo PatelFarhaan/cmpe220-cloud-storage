c77054817990c71d1615c20e213c96b394c450bc
Farhaan <patel.farhaaan@gmail.com>
2021-03-28 17:02:01 -0700

    admin panel and its features working

--- project/admin/admin_cred.py
+++ project/admin/admin_cred.py
@@ -1,16 +1,13 @@
 import sys
 sys.path.append('../../')
 
-
 from project import db
 from project.users.models import Users
 from werkzeug.security import generate_password_hash
 
-
-
-admin_obj = Users(email='darshan.mandlecha@sjsu.edu',
-                  hashed_password=generate_password_hash('Darshan'),
-                  mobile_number='6692927356',
+admin_obj = Users(email='***REMOVED_EMAIL***',
+                  hashed_password=generate_password_hash('***REMOVED_PASSWORD***'),
+                  mobile_number='6692048121',
                   is_admin=True
                   )
 db.session.add(admin_obj)
